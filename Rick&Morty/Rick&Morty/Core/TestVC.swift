9030ac7b24e6a871b34023d5c33c9e78ecec6530
Alexander <nefest71@gmail.com>
2023-10-23 16:35:43 +0300

    Project init

---
+++ Rick&Morty/Rick&Morty/Core/TestVC.swift
@@ -0,0 +1,15 @@
+//
+//  TestVC.swift
+//  Rick&Morty
+//
+//  Created by Alexander V. on 23.10.2023.
+//
+
+import UIKit
+
+final class TestVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
